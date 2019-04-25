50d684594eeb7260b342206057b8e8922aa615db
kelseyp1993 <kelsey.pritsker@gmail.com>
2019-04-25 16:34:36 -0700

    gradle

---
+++ src/main/java/io/pivotal/pal/calc/PalCalcApplication.java
@@ -0,0 +1,11 @@
+package io.pivotal.pal.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalCalcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PalCalcApplication.class, args);
+    }
+}
